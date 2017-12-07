946cbf4ae7228bab26516ac857734632b39de32f
venkativsr <31824741+venkativsr@users.noreply.github.com>
2017-12-07 15:15:14 +0530

    Created Pattern.cpp file from GITHub GUI
    This has a cpp program which displays a pattern.

---
+++ Pattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  int maxnumb=0;
+  cout<<"Enter the max number of lines you want to display the pattern"
+  cin >> maxnumb;
+  
+  for (int i=0;i<maxnumb;i++)
+  {
+    cout << "*" <<"/n"    
+  }
+}
